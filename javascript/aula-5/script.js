e38c464787965f51632240313b8f80d46836c35e
Gabriel Angelo <gabrielangelolira@gmail.com>
2025-04-28 21:59:15 -0300

    aula 6

--- javascript/aula-5/script.js
+++ javascript/aula-5/script.js
@@ -3,7 +3,7 @@ let frutas = ["Maçã", "Banana", "Laranja", "Uva"];
 let numeros = [1, 2, 3, 4, 5];
 let misto = ["Texto", 10, true];
 
-console.log(frutas);
+// console.log(frutas);
 // console.log(frutas.length);
 // console.log(frutas[3]);
 
@@ -19,55 +19,123 @@ console.log(frutas);
 //  Adicionar "Pera" no final da lista de Frutas.
 frutas.push("Pera");
 
-console.log(frutas);
+// console.log(frutas);
 
 // Pop()
 // Remove o ultimo item da lista de frutas
 frutas.pop();
-console.log(frutas);
+// console.log(frutas);
 
 // Shift()
 // Remove o primeiro item da lista de frutas
 frutas.shift();
 
-console.log(frutas);
+// console.log(frutas);
 
 // Unshift()
 // Adiciona um item no começo da lista de frutas
 
 frutas.unshift("Maçã mordida");
-console.log(frutas);
+// console.log(frutas);
 
 // Slice()
 // Pega uma parte do array
 
-let frutas2 = frutas.slice(0, 2)
-console.log(frutas2);
+let frutas2 = frutas.slice(0, 2);
+// console.log(frutas2);
 
 let frutas3 = frutas.slice(1, 4);
 
-console.log(frutas3);
+// console.log(frutas3);
 
+// console.log("frutas :: ", frutas);
 // Join()
 // Transforma o array em uma string
 let frutasString = frutas.join(" -> ");
-console.log(frutasString);
+// console.log(frutasString);
 
 // Reverse()
 // Inverte a ordem do array
 
 let frutasReverse = frutas.reverse();
-console.log(frutasReverse);
+// console.log(frutasReverse);
 
-// Concat() 
+// Concat()
 // Concatena dois arrays
 
 const frutasDoces = ["Melancia", "Manga", "Abacaxi"];
 const frutasSalgadas = ["Limão", "Tomate", "Cebola"];
 
-console.log("-----------------------")
+console.log("-----------------------");
 console.log(frutasDoces);
 console.log(frutasSalgadas);
 const frutasMisturadas = frutasDoces.concat(frutasSalgadas);
 
-console.log(frutasMisturadas);
+console.log(frutasMisturadas, frutasMisturadas.length);
+
+const cebola = frutasMisturadas[5];
+
+console.log(cebola);
+
+// IndexOf()
+// Retorna o index do item no array
+console.log(frutasMisturadas.indexOf("Gabriel"));
+
+// Includes()
+// Verifica se o item existe no array
+console.log(frutasMisturadas.includes("Abacaxi"));
+
+// Map()
+// Cria um novo array com base no array original
+const frutasEstragadas = frutasMisturadas.map((fruta) => {
+  return fruta + " Estragada";
+});
+
+console.log(frutasEstragadas);
+
+// Map com numeros
+const numerosDobrados = numeros.map((numero) => {
+  return numero * 2;
+});
+
+console.log("numeros ", numeros);
+console.log(numerosDobrados);
+
+const salarios = [1000, 2000, 3000, 4000, 5000];
+const salarioFormatado = salarios.map((salario) => {
+  // Usando toLocaleString
+  return salario.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+});
+
+console.log("salarios ", salarios);
+console.log(salarioFormatado);
+
+// Filter()
+// Cria um novo array com base no array original, mas filtrando os itens
+
+const maioresSalarios = salarios.filter((salario) => {
+  return salario < 2500;
+});
+
+console.log("maioresSalarios", maioresSalarios);
+
+// Find()
+// Retorna o primeiro item que satisfaz a condição
+
+const menorSalario = salarios.find((salario) => {
+  return salario < 2500;
+});
+
+console.info("menorSalario", menorSalario);
+
+// Reduce()
+// Retorna um único valor com base no array original
+
+const somaDeSalarios = salarios.reduce((valorAnterior, atual) => {
+    return valorAnterior + atual;
+}, 7000)
+
+console.log("somaDeSalarios", somaDeSalarios);
